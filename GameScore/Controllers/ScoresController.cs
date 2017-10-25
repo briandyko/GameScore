697979891c5cd453f6feb24a792ca47726eeaf86
briandyko <briandyko@gmail.com>
2017-10-25 15:25:56 -0400

    added Team to controller and view

--- GameScore/Controllers/ScoresController.cs
+++ GameScore/Controllers/ScoresController.cs
@@ -78,7 +78,7 @@ namespace GameScore.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Name,Points")] Score score)
+        public ActionResult Edit([Bind(Include = "ID,Name,Points,Team")] Score score)
         {
             if (ModelState.IsValid)
             {
